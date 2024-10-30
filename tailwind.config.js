ddffe7469099e9ca936c46faf80b5cb8afd0df6c
kambatukebele <kambacharles11@gmail.com>
2024-10-30 13:14:58 +0100

    added center propriety, and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,27 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
+        container: {
+            center: true,
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                lora: ["Lora", "serif"],
+                open_sans: ["Open Sans", "sans-serif"],
+                montserrat: ["Montserrat", "sans-serif"],
+                poppins: ["Poppins", "sans-seri"],
+                merriweather: ["Merriweather", "serif"],
+                roboto: ["Roboto", "sans-serif"],
             },
         },
     },
